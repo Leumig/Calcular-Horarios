3af3888c3d09a71b7ef7697e92c4ce69e34be862
Leumig <junmigue7@gmail.com>
2024-03-01 01:04:33 -0300

    boton copiar y animaciones

--- js/logica.js
+++ js/logica.js
@@ -1,25 +1,30 @@
 const boton = document.getElementById("calcular");
 const texto = document.getElementById("texto");
+const botonCopiar = document.getElementById("botonCopiar");
+
 texto.innerHTML = "&#x2764 Hola Gibeeeee, te amo mucho &#x2764";
+let textoCalculado = false;
 
 function calcularHoraSegunPais(hora, minutos, diferenciaHoraria) {
     const fecha = new Date();
-    fecha.setHours(parseInt(hora));
-    fecha.setMinutes(parseInt(minutos));
-    fecha.setHours(fecha.getHours() + diferenciaHoraria);
+     fecha.setHours(parseInt(hora));
+     fecha.setMinutes(parseInt(minutos));
+     fecha.setHours(fecha.getHours() + diferenciaHoraria);
 
-    const nuevaHora = fecha.getHours().toString().padStart(2, "0");
-    const nuevosMinutos = fecha.getMinutes().toString().padStart(2, "0");
+     const nuevaHora = fecha.getHours().toString().padStart(2, "0");
+     const nuevosMinutos = fecha.getMinutes().toString().padStart(2, "0");
 
-    return `${nuevaHora}:${nuevosMinutos} hrs.`;
+     return `${nuevaHora}:${nuevosMinutos} hrs.`;
 }
 
-boton.addEventListener("click", ()=> {
+boton.addEventListener("click", () => {
     const horaRecibida = document.getElementById("hora").value;
     const [hora, minutos] = horaRecibida.split(":");
-    
+
     if (horaRecibida === "") {
-        texto.innerHTML = "&#x2764 Pero amor, primero tenes que escribir la hora (con los minutos) &#x2764";
+        texto.innerHTML =
+        "&#x2764 Pero amor, primero tenes que escribir la hora (con los minutos) &#x2764";
+        textoCalculado = false;
     } else {
         texto.innerHTML = `
             Argentina: ${calcularHoraSegunPais(hora, minutos, 2)}<br>
@@ -35,28 +40,34 @@ boton.addEventListener("click", ()=> {
             Paraguay: ${calcularHoraSegunPais(hora, minutos, 2)}<br>
             Perú: ${horaRecibida} hrs.<br>
             Uruguay: ${calcularHoraSegunPais(hora, minutos, 2)}<br>`;
+
+        textoCalculado = true;
+     }
+});
+
+const mensaje = document.querySelector("#mensaje");
+
+botonCopiar.addEventListener("click", function () {
+    const textoGenerado = document.getElementById("texto");
+
+    if (textoCalculado == false)
+    {
+        textoGenerado.innerHTML = "&#x2764 Pero qué estás copiandoooo &#x2764";
+        textoCalculado == false
+    } else {
+        const seleccion = window.getSelection();
+        const rango = document.createRange();
+        rango.selectNodeContents(textoGenerado);
+        seleccion.removeAllRanges();
+        seleccion.addRange(rango);
+        document.execCommand("copy");
+        seleccion.removeAllRanges();
+    
+        mensaje.innerHTML = "\u2714";
+        mensaje.classList.add("fade-in");
+        setTimeout(function () {
+        mensaje.classList.remove("fade-in");
+        mensaje.classList.add("fade-ou");
+        }, 1500);
     }
 });
-
-/*
-const botonCopiarTexto = document.getElementById("copiarTexto");
-
-botonCopiarTexto.addEventListener("click", () => {
-    // Seleccionar el texto generado
-    const textoGenerado = document.getElementById("texto");
-    const seleccion = window.getSelection();
-    const rango = document.createRange();
-    rango.selectNodeContents(textoGenerado);
-    seleccion.removeAllRanges();
-    seleccion.addRange(rango);
-
-    // Copiar el texto seleccionado
-    document.execCommand("copy");
-
-    // Limpiar la selección
-    seleccion.removeAllRanges();
-
-    // Mostrar mensaje de confirmación
-    alert("El texto ha sido copiado al portapapeles.");
-});
-*/
